40a0642846daedd9de573e6c2c30b71127d4caa5
ferlucerooo <ferlucero2210@gmail.com>
2024-05-01 18:48:21 -0300

    hbs & socket: me falto mostar el producto agregado y eliminarlo

--- src/app.js
+++ src/app.js
@@ -1,19 +1,61 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
+import { Server } from 'socket.io';
 import config from './config.js';
 import productsRoutes from './routes/products.routes.js';
 import cartsRoutes from './routes/carts.routes.js';
+import viewsRoutes from './routes/views.router.js';
 
 const app = express ();
 
-
 app.use(express.json());             // con esto express es capas de entender la solicitud tipo post con body(json)
 app.use(express.json({extended: true}));
+// motor de plantillas
+app.engine('handlebars', handlebars.engine());
+app.set('views', `${config.DIRNAME}/views`);
+app.set('view engine','handlebars');
+//Rutas
 app.use('/api/products',productsRoutes);
 app.use('/api/carts', cartsRoutes);
 app.use('/static', express.static(`${config.DIRNAME}/public`));
+app.use('/',viewsRoutes)
 
 
 // Puerto donde escucha el servidor 
-app.listen(config.PORT, ()=>{
+const httpServer = app.listen(config.PORT, ()=>{
     console.log(`Servidor Express activo en el puerto ${config.PORT}`);
 });
+
+const socketServer = new Server(httpServer);
+
+// El método set() nos permite setear objetos globales para nuestra app.
+// En este caso lo aprovechamos para socketServer, que luego recuperaremos
+// desde los endpoints donde necesitemos publicar mensajes Websockets.
+app.set('socketServer', socketServer);
+
+let products = [];
+
+//listener: escucho eventos de coneccion
+socketServer.on('connection',client =>{
+    console.log(`Cliente conectado, id ${client.id} desde ${client.handshake.address}`);
+
+    //el server logra mostrar el mensaje, emite
+    client.on('newMessage', data => {
+        Message.push(data);
+        console.log(`Mensaje recibido desde ${client.id}: ${data}`);
+        client.emit('newMessageConfirmation', data);
+    });
+
+    /* client.on('createProduct', formData => {
+        console.log('Producto agregado correctamente(server)');
+        client.emit('newProduct', formData);
+    }) */
+
+    client.on('form_product', (data) => {
+        console.log(data);
+        products.push(data);
+        socketServer.emit('newProduct',products)
+    });
+
+})
+
---
+++ src/routes/views.router.js
@@ -0,0 +1,57 @@
+import {Router} from 'express';
+import ProductManager from '../productManager.js';
+
+const router = Router ();
+const productManager = new ProductManager('products.json');
+
+router.get('/bienvenida', (req,res)=>{
+    const user ={
+        firstName: 'carlos'
+    }
+    res.render('index',user )
+})
+
+router.get('/chat', (req,res)=>{
+    res.render('chat', {});
+})
+
+
+router.get('/home', async (req,res)=>{
+    try{
+        const products = await productManager.getProducts();
+        res.render('home', {products});
+
+    } catch (error){
+        console.error('Error al obtener los productos:', error);
+    };
+})
+
+router.get('/realtimeproducts', async (req,res)=>{
+    try{
+        const products = await productManager.getProducts();
+        res.render('realTimeProducts', {products});
+
+    } catch (error){
+        console.error('Error al obtener los productos:', error);
+    };
+})
+router.post('/realtimeproducts', async (req,res)=>{
+    try{
+        const socketServer = req.app.get('socketServer');
+        const newProduct = req.body;
+
+        const add = await productManager.addProduct(newProduct);
+        console.log({add});
+
+        socketServer.emit('newProduct', add)
+        //res.render('realTimeProducts', {newProduct});
+        const updatedProducts = await productManager.getProducts();
+        res.status(200).render('realTimeProducts',{products: updatedProducts});
+
+    }catch(error){
+        console.error('Error al agregar el producto:',error);
+    }
+})
+
+
+export default router;
